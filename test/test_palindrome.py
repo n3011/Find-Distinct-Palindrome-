9c97d38bd4bb2d1dc515ffc486fac1a75c455019
n3011 <mrinal.haloi11@gmail.com>
2016-03-22 14:25:03 +0800

    test addition

---
+++ test/test_palindrome.py
@@ -0,0 +1,20 @@
+import unittest
+from app.mclaren import Palindrome
+import numpy as np
+
+
+class TestPalindrome(unittest.TestCase):
+    def setUp(self):
+        self.plnd = Palindrome()
+
+    def test_Longest_palindrome(self):
+        result = self.plnd.getLongestPalindrome('sqrrqabccbatudefggfedvwhijkllkjihxymnnmzpop')
+        self.assertEqual(result[2], 'hijkllkjih')
+    def test_distinct_palindrome(self):
+        self.plnd.getDistinct('sqrrqabccbatudefggfedvwhijkllkjihxymnnmzpop', 0)
+        maxindices = np.argsort(self.plnd.plength)[-3:] 
+        self.assertEqual(self.plnd.palindromes[maxindices[0]], 'abccba')
+        self.assertEqual(self.plnd.palindromes[maxindices[1]], 'defggfed')
+        self.assertEqual(self.plnd.palindromes[maxindices[2]], 'hijkllkjih')
+    def test_string_input(self):
+        self.assertRaises(ValueError, self.plnd.getLongestPalindrome, '27sgyrk')
